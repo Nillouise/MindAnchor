b773738d16abb32f356562bacb88220a08dbdaf1
Nillouise <LovelyNihilism@outlook.com>
2017-08-02 02:16:33 +0800

    解决spring注入服务

--- src/main/java/Nillouise/listener/SessionListener.java
+++ src/main/java/Nillouise/listener/SessionListener.java
@@ -1,4 +1,4 @@
-package Nillouise.controller;
+package Nillouise.listener;
 
 
 import Nillouise.model.UserInfo;
@@ -9,27 +9,27 @@ import com.mysql.cj.api.Session;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 import java.util.List;
 
-@Controller
+
+
 public class SessionListener implements HttpSessionListener
 {
 
     //这里好像不能用spring的注入 https://stackoverflow.com/questions/11718216/httpsessionlistener-using-spring-injections-and-services-impossible-to-access
     //这里搞得连 UserInfoService 里面的 UserInfoMapper 都不能正常注入了。。。
 //    @Autowired
-    private UserInfoService userInfoService;
+//    private UserInfoService userInfoService;
 
-    public SessionListener()
-    {
-        userInfoService = new UserInfoServiceImpl();
-    }
+
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SessionListener.class);
     public void sessionCreated(HttpSessionEvent httpSessionEvent)
@@ -39,6 +39,9 @@ public class SessionListener implements HttpSessionListener
 
     public void sessionDestroyed(HttpSessionEvent httpSessionEvent)
     {
+        UserInfoService userInfoService = getUserInfoService(httpSessionEvent);
+
+
         LOGGER.info("destroy session "+httpSessionEvent);
         HttpSession session = httpSessionEvent.getSession();
         UserInfo userInfo = (UserInfo)session.getAttribute(StringConstant.userInfo);
@@ -48,4 +51,23 @@ public class SessionListener implements HttpSessionListener
         }
         return;
     }
+
+    private UserInfoService getUserInfoService(HttpSessionEvent httpSessionEvent)
+    {
+
+        HttpSession session = httpSessionEvent.getSession();
+
+        ApplicationContext ctx =
+                WebApplicationContextUtils.
+                        getWebApplicationContext(session.getServletContext());
+
+        // 通过这种用web的功能获取spring的context，然后从context中获得bean对象，默认的userInfoService名字就叫userInfoServiceImpl（跟我定义的impl类名一样）
+        //貌似不能用自己在spring的xml文件里在建一个同样类型的对象（貌似是单例的）
+        UserInfoService counterService =
+                (UserInfoService) ctx.getBean("userInfoServiceImpl");
+
+        return counterService;
+    }
+
+
 }
